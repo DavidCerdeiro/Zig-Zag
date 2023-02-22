9c1cf24695f93d1cf6d210600d07967d80e0d3c2
DESKTOP-101Q7TS\34684 <candido.16.alonso@gmail.com>
2023-02-22 19:57:07 +0100

    escena inicio

--- Practica2/Assets/Scripts/Pulsar.cs
+++ Practica2/Assets/Scripts/Pulsar.cs
@@ -3,60 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Pulsar : MonoBehaviour
+public class pulsar : MonoBehaviour
 {
-    private Button boton;
+    //public Button bnt;
+    private Button btn;
     public Image img;
     private bool contar;
     private int numero;
     public Sprite[] spNumeros;
     public Text texto;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        //boton = GameObject.FindAnyObjectByType<Button>();
-        boton = GameObject.FindWithTag("BtnPulsar").GetComponent<Button>();
-        boton.onClick.AddListener(Pulsado);
+        //btn = GameObject.FindAnyObjectByType<Button>();
+        btn = GameObject.FindWithTag("boton").GetComponent<Button>();
+        btn.onClick.AddListener(Pulsado);
         contar = false;
         numero = 3;
     }
 
-    void Pulsado(){
-        texto.gameObject.SetActive(true);
-        boton.gameObject.SetActive(false);
+    void Pulsado()
+    {
+        img.gameObject.SetActive(true);
+        //texto.gameObject.SetActive(true);
+        btn.gameObject.SetActive(false);
         contar = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(contar){
-            switch(numero){
-                case 0:
-                    Debug.Log("Aqui se cambia de escena");
-                    break;
-                case 1:
-                    //img.sprite = spNumeros[0];
-                    texto.text = "1";
-                    break;
-                case 2:
-                    //img.sprite = spNumeros[1];
-                    texto.text = "2";
-                    break;
-                case 3:
-                    //img.sprite = spNumeros[2];
-                    texto.text = "3";
-                    break;
+        if (contar) 
+        {
+            switch (numero)
+            {
+                case 0: Debug.Log("Terminado - Salto a otra escena"); break;
+                case 1: img.sprite = spNumeros[0]; /*texto.text = "1"*/; break;
+                case 2: img.sprite = spNumeros[1]; /*texto.text = "2"*/; break;
+                case 3: img.sprite = spNumeros[2]; /*texto.text = "3"*/; break;
             }
-            StartCoroutine(Esperar());
+            StartCoroutine(Esperar());  //proceso paralelo
             contar = false;
             numero--;
         }
     }
 
-    IEnumerator Esperar(){
+    IEnumerator Esperar()
+    {
         yield return new WaitForSeconds(1);
         contar = true;
+
     }
 }
