e1b17f519c57f33c1d654c45539734279b8dea27
DESKTOP-101Q7TS\34684 <candido.16.alonso@gmail.com>
2023-02-23 12:02:50 +0100

    Revert "musica fondo y cambio de escenas"
    This reverts commit c1849478b361ac560a9e2f77ec28d263d8246418.

--- Practica2/Assets/Scripts/Pulsar.cs
+++ Practica2/Assets/Scripts/Pulsar.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class pulsar : MonoBehaviour
 {
@@ -39,7 +38,7 @@ public class pulsar : MonoBehaviour
         {
             switch (numero)
             {
-                case 0: Debug.Log("Terminado - Salto a otra escena"); SceneManager.LoadScene("Escena2", LoadSceneMode.Single); break;
+                case 0: Debug.Log("Terminado - Salto a otra escena"); break;
                 case 1: img.sprite = spNumeros[0]; /*texto.text = "1"*/; break;
                 case 2: img.sprite = spNumeros[1]; /*texto.text = "2"*/; break;
                 case 3: img.sprite = spNumeros[2]; /*texto.text = "3"*/; break;
