2230d096561a949279f547e0ad8444a5e1ec3d91
DESKTOP-TBSN9A2\raule <raulero2002@gmail.com>
2023-02-23 21:12:34 +0100

    Movimiento de jugador, muerte del personaje por caida, intento de generación de suelo

--- Practica2/Assets/Scripts/Movimiento.cs
+++ Practica2/Assets/Scripts/Movimiento.cs
@@ -5,33 +5,53 @@ using UnityEngine;
 public class Movimiento : MonoBehaviour
 {
     public Camera camara;
-    private int velocidad = 1;
+    private int velocidad;
     private Vector3 offset;
     private float valZ;
     private float valX;
+    private Rigidbody rb;
+    private int limite;
     public GameObject prefabSuelo;
     // Start is called before the first frame update
     void Start()
     {
-        velocidad = 1;
+        velocidad = 2;
         offset = camara.transform.position;
         valX = 0.0f;
         valZ = 0.0f;
+        limite = 10;
         SueloInicial();
+        rb = GetComponent<Rigidbody>();
     }
 
     void SueloInicial(){
         
-        for(int n = 0; n<3; n++){
+        for(int n = 0; n<limite; n++){
             valX += 6.0f;
             //valZ += 6.0f;
-            GameObject elsuelo = Instantiate(prefabSuelo, new Vector3(valX, 0.0f, valZ), Quaternion.identity) as GameObject;
+            Vector3 position = new Vector3(valX, 0.0f, Random.Range((valZ-3), (valZ+3)));
+            GameObject elsuelo = Instantiate(prefabSuelo, position, Quaternion.identity) as GameObject;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float movVertical = Input.GetAxis("Horizontal");
+        Vector3 movimiento = new Vector3(1.0f, 0.0f, 4*(-movVertical));
+        rb.AddForce(movimiento * velocidad);
         camara.transform.position = this.transform.position + offset;
+
+        if (transform.position.x%6 == 0)
+        {
+            Debug.Log("Nueva plataforma");
+            limite = limite + 1;
+        }
+
+        if (transform.position.y < -2)
+        {
+            Debug.Log("Jugador ha muerto");
+            Destroy(this.gameObject);
+        }
     }
 }
