b1ea494c95d252f40cbf11ddf432775e935deecd
DESKTOP-101Q7TS\34684 <candido.16.alonso@gmail.com>
2023-02-24 14:37:08 +0100

    prefab moneda con particulas y sonido al chocar

---
+++ Practica2/Assets/Scripts/monedaColision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monedaColision : MonoBehaviour
+{
+    public AudioSource sonidoMoneda;
+    public ParticleSystem particulas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")){
+            particulas.transform.position = this.gameObject.transform.position;
+            sonidoMoneda.Play();
+            particulas.Play();
+            Destroy(this.gameObject);
+        };
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
